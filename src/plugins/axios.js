bb07178e2fe0f20696f0ed415be5ab08fef9b9ed
Danillo De Siena Okazaki <danillo.s.okazaki@gmail.com>
2026-05-13 19:04:14 -0300

    fix axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 ﻿import Vue from 'vue'
 import axios from 'axios'
 
-const apiBaseUrl = process.env.VUE_APP_URL_SERVIDOR_DADOS || 'http://45.231.133.250:3000'
+const apiBaseUrl = process.env.VUE_APP_URL_SERVIDOR_DADOS || '204.157.124.146:3000'
 
 const api = axios.create({
 	baseURL: apiBaseUrl,
